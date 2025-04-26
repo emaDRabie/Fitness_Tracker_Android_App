1ac5981f952fe7934022ef20866f2e979d0d7217
emaDRabie <emadrabie046@gmail.com>
2025-04-26 19:30:52 +0300

    changed

--- Fitnessy/app/src/main/java/com/crowcode/fitnessy/MainActivity.kt
+++ Fitnessy/app/src/main/java/com/crowcode/fitnessy/MainActivity.kt
@@ -5,19 +5,29 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.crowcode.fitnessy.onboard.OnboardingScreen
 import com.crowcode.fitnessy.onboard.OnboardingUtils
 import com.crowcode.fitnessy.ui.theme.FitnessyTheme
@@ -42,28 +52,31 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     private fun HomeScreen() {
-        val context = LocalContext.current
-        val intent = Intent(context, HomeActivity::class.java)
+//        val context = LocalContext.current
+//        val intent = Intent(context, HomeActivity::class.java)
 
         Scaffold(
             modifier = Modifier.fillMaxSize()
         ) { innerPadding ->
             Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(innerPadding)
             ) {
-                Text(
-                    text = "Home Screen",
-                    style = MaterialTheme.typography.headlineLarge,
-                    modifier = Modifier.clickable {
-                        context.startActivity(intent)
-                    }
+                Image(
+                    painter = painterResource(id = R.drawable.home),
+                    contentDescription = "just an image",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
                 )
                 Text(
-                    text = "Emad Rabie",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(top = 20.dp)
+                    text = "Welcome To Fitnessy",
+                    style = TextStyle(
+                        fontWeight = FontWeight.W600,
+                        fontSize = 32.sp
+                    )
                 )
+
             }
         }
     }
@@ -75,7 +88,7 @@ class MainActivity : AppCompatActivity() {
             onboardingUtils.setOnboardingCompleted()
             scope.launch {
                 setContent {
-                        HomeScreen()
+                    HomeScreen()
                 }
             }
         }
@@ -84,6 +97,6 @@ class MainActivity : AppCompatActivity() {
     @Preview(showBackground = true)
     @Composable
     fun HomeScreenPreview() {
-            HomeScreen()
+        HomeScreen()
     }
 }
