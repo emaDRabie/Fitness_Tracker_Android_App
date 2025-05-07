91c8efada3f5b882ca33df81d90e20015bd54dad
emaDRabie <emadrabie046@gmail.com>
2025-05-08 01:15:10 +0300

    user setup screen finished :)

--- Fitnessy/app/src/main/java/com/crowcode/fitnessy/user/Trainee.kt
+++ Fitnessy/app/src/main/java/com/crowcode/fitnessy/user/Trainee.kt
@@ -3,12 +3,13 @@ package com.crowcode.fitnessy.user
 import java.util.Date
 
 class Trainee(
-    val name: String,
-    val dob: Date,
-    val weight: Float,
-    val height: Float,
-    val gender: String,
-    val waterGoal: Float,
-    val calGoal: String,
-    val weightGoal: String,
+    val name: String = "",
+    val dob: Date = Date(),
+    val weight: Float = 0f,
+    val height: Float = 0f,
+    val gender: String = "",
+    val waterGoal: Float = 0f,
+    val calGoal: String = "",
+    val weightGoal: String = "",
+    val id: String = "",
 )
--- Fitnessy/app/src/main/java/com/crowcode/fitnessy/user/UserSetupActivity.kt
+++ Fitnessy/app/src/main/java/com/crowcode/fitnessy/user/UserSetupActivity.kt
@@ -1,21 +1,103 @@
 package com.crowcode.fitnessy.user
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.crowcode.fitnessy.R
+import com.crowcode.fitnessy.databinding.ActivityUserSetupBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class UserSetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityUserSetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_user_setup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // date picker
+        val calendar = Calendar.getInstance()
+        binding.dobEt.setOnClickListener {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+                binding.dobEt.setText(selectedDate)
+            }, year, month, day)
+
+            datePicker.show()
+        }
+
+
+        binding.confirmButton.setOnClickListener {
+            binding.progress.isVisible = true
+
+            val name = binding.displayNameEt.text.toString()
+            val dobStr = binding.dobEt.text.toString()
+            val weightStr = binding.weightEt.text.toString()
+            val heightStr = binding.heightEt.text.toString()
+            val waterStr = binding.waterEt.text.toString()
+            val cal = binding.calEt.text.toString()
+            val tWeight = binding.tWeightEt.text.toString()
+            val selectedGenderId = binding.toggleGender.checkedRadioButtonId
+            val selectedGender = findViewById<RadioButton>(selectedGenderId)?.text.toString()
+
+            if (name.isBlank() || dobStr.isBlank() || weightStr.isBlank() ||
+                heightStr.isBlank() || waterStr.isBlank() || cal.isBlank() || tWeight.isBlank()
+            ) {
+                Toast.makeText(this, "Empty Fields", Toast.LENGTH_SHORT).show()
+                binding.progress.isVisible = false
+                return@setOnClickListener
+            }
+
+            try {
+                val dob = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse(dobStr)
+                val trainee = Trainee(
+                    name = name,
+                    dob = dob ?: Date(),
+                    weight = weightStr.toFloat(),
+                    height = heightStr.toFloat(),
+                    gender = selectedGender,
+                    waterGoal = waterStr.toFloat(),
+                    calGoal = cal,
+                    weightGoal = tWeight,
+                )
+
+                val db = Firebase.firestore
+                db.collection("users")
+                    .add(trainee)
+                    .addOnSuccessListener {
+                        it.update("id", it.id)
+                        Toast.makeText(this, "Added successfully", Toast.LENGTH_SHORT).show()
+                        binding.progress.isVisible = false
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Failed: ${it.message}", Toast.LENGTH_SHORT).show()
+                        binding.progress.isVisible = false
+                    }
+
+            } catch (e: Exception) {
+                Toast.makeText(this, "Invalid input: ${e.message}", Toast.LENGTH_SHORT).show()
+                binding.progress.isVisible = false
+            }
+        }
+
     }
 }
